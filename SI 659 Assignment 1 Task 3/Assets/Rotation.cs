4b6923e5b24e7bf27e240682f618ed0d1ef983fd
Xizi Wang <xiziwang@umich.edu>
2019-01-25 04:17:01 -0500

    upload a video & codes of task 3.1, 3.2 and III

---
+++ SI 659 Assignment 1 Task 3/Assets/Rotation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    Renderer rederer;
+    float rotationSpeed = 20.0f;
+    float currentAngle = -90.0f;
+    float minAngle = -90.0f;
+    float maxAngle = 0.0f;
+    bool clockwise = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rederer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float angle = Time.deltaTime * rotationSpeed;
+
+        if (currentAngle >= maxAngle)
+        {
+            clockwise = false;
+        }
+        else if(currentAngle <= minAngle) {
+            clockwise = true;
+        }
+        if (clockwise)
+        {
+            currentAngle += angle;
+        }
+        else {
+            currentAngle -= angle;
+
+        }
+        transform.rotation = Quaternion.Euler(0, currentAngle, 0);
+    }
+}
---
+++ SI 659 Assignment 1 Task 3/Assets/SetColor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetColor : MonoBehaviour {
+	public GameObject wallL;
+	public GameObject wallM;
+	public GameObject wallR;
+	public GameObject floor;
+
+	// Use this for initialization
+	void Start () {
+		wallL.GetComponent<Renderer>().material.SetColor("_Color", new Color(152f/225f,166f/225f,200f/225f,1f));
+		wallM.GetComponent<Renderer>().material.SetColor("_Color", new Color(152f/225f,166f/225f,200f/225f,1f));
+		wallR.GetComponent<Renderer>().material.SetColor("_Color", new Color(152f/225f,166f/225f,200f/225f,1f));
+		floor.GetComponent<Renderer>().material.SetColor("_Color", new Color(165f/225f,147f/225f,141f/225f,1f));
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
