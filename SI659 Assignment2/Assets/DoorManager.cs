5fd1656ac06fff099dcd6f2a3416fc1e72ca3509
Wang <xiziwang@UMICH.EDU>
2019-02-18 20:46:58 -0500

    upload assignment2

---
+++ SI659 Assignment2/Assets/DoorManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour {
+    public GameObject player;
+    Vector3 playerLocation;
+
+    Renderer rederer;
+    float rotationSpeed = 40.0f;
+    float currentAngle = -90.0f;
+    float minAngle = -90.0f;
+    float maxAngle = 0.0f;
+    bool isOpening = false;
+    bool isClosing = true;
+
+    public AudioSource doorAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rederer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetPlayerLocation();
+        DoorListener();
+    }
+
+    void GetPlayerLocation()
+    {
+        playerLocation = player.transform.position;
+    }
+
+    void DoorListener()
+    {
+        // if the user is in front of the door
+        if(playerLocation.x>=0 && playerLocation.x <=10 && playerLocation.z<=-12 && playerLocation.z >= -17)
+        {
+            if (!isOpening)
+            {
+                doorAudio.Play();
+            }
+            OpenDoor();
+        }
+        else
+        {
+            if (!isClosing)
+            {
+                doorAudio.Play();
+            }
+            CloseDoor();
+        }
+    }
+
+    void OpenDoor()
+    {
+        float angle = Time.deltaTime * rotationSpeed;
+
+        if (currentAngle <= maxAngle)
+        {
+            currentAngle += angle;
+            transform.rotation = Quaternion.Euler(0, currentAngle, 0);
+        }
+        isOpening = true;
+        isClosing = false;
+    }
+
+    void CloseDoor()
+    {
+        float angle = Time.deltaTime * rotationSpeed;
+
+        if (currentAngle >= minAngle)
+        {
+            currentAngle -= angle;
+            transform.rotation = Quaternion.Euler(0, currentAngle, 0);
+        }
+        isClosing = true;
+        isOpening = false;
+    }
+}
---
+++ SI659 Assignment2/Assets/SetColor.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetColor : MonoBehaviour {
+	public GameObject wallL1;
+    public GameObject wallL2;
+    public GameObject wallL3;
+
+    public GameObject wallM1;
+    public GameObject wallM2;
+    public GameObject wallM3;
+    public GameObject wallM4;
+
+    public GameObject wallR1;
+    public GameObject wallR2;
+    public GameObject wallR3;
+
+    public GameObject celling;
+
+    //public GameObject floor;
+
+    // Use this for initialization
+    void Start () {
+		wallL1.GetComponent<Renderer>().material.SetColor("_Color", new Color(152f/225f,166f/225f,200f/225f,1f));
+        wallL2.GetComponent<Renderer>().material.SetColor("_Color", new Color(152f / 225f, 166f / 225f, 200f / 225f, 1f));
+        wallL3.GetComponent<Renderer>().material.SetColor("_Color", new Color(152f / 225f, 166f / 225f, 200f / 225f, 1f));
+
+        wallM1.GetComponent<Renderer>().material.SetColor("_Color", new Color(152f/225f,166f/225f,200f/225f,1f));
+        wallM2.GetComponent<Renderer>().material.SetColor("_Color", new Color(152f / 225f, 166f / 225f, 200f / 225f, 1f));
+        wallM3.GetComponent<Renderer>().material.SetColor("_Color", new Color(152f / 225f, 166f / 225f, 200f / 225f, 1f));
+        wallM4.GetComponent<Renderer>().material.SetColor("_Color", new Color(152f / 225f, 166f / 225f, 200f / 225f, 1f));
+
+        wallR1.GetComponent<Renderer>().material.SetColor("_Color", new Color(152f/225f,166f/225f,200f/225f,1f));
+        wallR2.GetComponent<Renderer>().material.SetColor("_Color", new Color(152f / 225f, 166f / 225f, 200f / 225f, 1f));
+        wallR3.GetComponent<Renderer>().material.SetColor("_Color", new Color(152f / 225f, 166f / 225f, 200f / 225f, 1f));
+
+        celling.GetComponent<Renderer>().material.SetColor("_Color", new Color(152f / 225f, 166f / 225f, 200f / 225f, 1f));
+
+
+        //floor.GetComponent<Renderer>().material.SetColor("_Color", new Color(165f/225f,147f/225f,141f/225f,1f));
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
