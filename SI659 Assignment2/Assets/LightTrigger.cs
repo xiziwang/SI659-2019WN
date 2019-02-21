b387a75b741cf1e9d54369caa8f1fa150083266b
Wang <xiziwang@UMICH.EDU>
2019-02-20 20:45:25 -0500

    removed old folder and update assignment

---
+++ SI659 Assignment2/Assets/LightTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTrigger : MonoBehaviour {
+    public GameObject leftLight;
+    public GameObject rightLight;
+    private void OnTriggerEnter(Collider other)
+    {
+        leftLight.SetActive(true);
+        rightLight.SetActive(true);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        leftLight.SetActive(false);
+        rightLight.SetActive(false);
+    }
+}
